7e9558c1f8a869f34bff5a58b5c5e833d74eb7d7
user <user@TapasRout>
2023-02-14 16:59:58 +0530

    Intersect between 2 Arrays

---
+++ CoreJavaExamples/src/com/tapas/java/InterSectElement.java
@@ -0,0 +1,20 @@
+
+//Intersect element from 2 Arrays// Find out the duplicate values from 2 Arrays
+package com.tapas.java;
+
+import java.util.Arrays;
+
+public class InterSectElement {
+
+	public static void main(String[] args) {
+		
+		int firstArray[] = {1,2,3,2,1};
+		int [] secondArray = {2,2};
+		//using Stram Function
+		//convert Arrays to stream function
+		Arrays.stream(firstArray).filter(x->Arrays.stream(secondArray).anyMatch(y->y==x)).
+		distinct().forEach(System.out::println);
+
+	}//main
+
+}//class
