4c3ee8bdf388e5191e3b27911a65de77c2abbb6c
user <user@TapasRout>
2023-06-10 07:12:47 +0530

    update

--- CoreJavaExamples/src/com/tapas/java/BalanceParanthesis.java
+++ CoreJavaExamples/src/com/tapas/java/BalanceParanthesis.java
@@ -1,11 +1,15 @@
 package com.tapas.java;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
+
+import javax.management.Query;
 
 public class BalanceParanthesis {
 
 	public static void main(String[] args) {
-		String s = "{[()]";
+		String s = "{[()]}";
 		if (isBalance(s)) {
 			System.out.println(s + " Is a balanced Paranthesis");
 		} else
@@ -13,13 +17,14 @@ public class BalanceParanthesis {
 	}
 
 	public static boolean isBalance(String s) {
-		Stack<Character> stack = new Stack();
+		//Stack<Character> stack = new Stack<>(); //this is a Synchronized 
+		Deque<Character> stack = new ArrayDeque<>(); // this is a unsynchronized
 		for (int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
 			if (c == '(' || c == '{' || c == '[') {
 				stack.push(c);
 			} else if (c == ')' || c == '}' || c == ']') {
-				if (stack.empty()) {
+				if (stack.isEmpty()) {
 					return false;
 				}
 				char top = stack.pop();
